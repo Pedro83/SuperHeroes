86b0029a932c7e4f9ebd4480088d51b7c914a36a
Pedro Capo <pedrocapolozano@gmail.com>
2025-03-21 10:35:09 +0100

    Search siendo su propio componente y utilizacion de la metodologia BEM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
